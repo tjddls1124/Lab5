b9090e74f8d5614aaa8cd840da642e893b068e84
tjddls1124 <tjddls1124@nate.com>
2017-04-06 03:47:14 +0900

    끝!

--- app/src/main/java/com/example/sungin/lab5/Table.java
+++ app/src/main/java/com/example/sungin/lab5/Table.java
@@ -22,25 +22,52 @@ public class Table {
     public Table(String t){
         Tablename = t;
         is_void = true;
+        year = 0;
+        month = 0;
+        day = 0;
+        hour =0;
+        min = 0;
+        spa_num = null;
+        piz_num = null;
+        vip_membership = false;
+        total_price = 0;
     }
 
-    public Table putTable(int year, int month, int day, int hour, int min,String spa_num,String piz_num,boolean vip_membership){
-        this.year = year;
-        this.month = month;
-        this.day = day;
-        this.hour = hour;
-        this.min = min;
+    public void putTableDate(Table t , int year, int month, int day) {
+        t.year = year;
+        t.month = month;
+        t.day = day;
+    }
+    public void putTableTime (Table t,  int hour, int min)    {
+        t.hour = hour;
+        t.min = min;
+    }
+    public void putTableMenu (Table t,String spa_num,String piz_num,boolean vip_membership){
 
-        this.spa_num = spa_num;
-        int spa_numi= Integer.getInteger(spa_num);
-        this.piz_num = piz_num;
-        int piz_numi = Integer.getInteger(piz_num);
-        this.vip_membership = vip_membership;
-        if(vip_membership == true ) this.total_price =   (int) ( ((10000 * spa_numi) + (12000 * piz_numi)) * 0.7);
-        else this.total_price =   (int) ( ( (10000 * spa_numi) + (12000 * piz_numi)) * 0.9);
+        t.spa_num = spa_num;
+        int spa_numi= Integer.parseInt(spa_num);
+        t.piz_num = piz_num;
+        int piz_numi = Integer.parseInt(piz_num);
+        t.vip_membership = vip_membership;
+        if(vip_membership == true ) this.total_price =  (int) (((10000 * spa_numi) + (12000 * piz_numi)) * 0.7);
+        else t.total_price =    (int) (( (10000 * spa_numi) + (12000 * piz_numi)) * 0.9);
 
-        this.is_void = false;
+        t.is_void = false;
+    }
 
-        return this;
+    public void putTableData(Table receiver, Table sender){
+        receiver.Tablename = sender.Tablename;
+        receiver.is_void =sender.is_void;
+        receiver.year = sender.year;
+        receiver.month=sender.month;
+        receiver.day = sender.day;
+        receiver.hour = sender.hour;
+        receiver.min = sender.min;
+        receiver.spa_num = sender.spa_num;
+        receiver.piz_num = sender.piz_num;
+        receiver.vip_membership = sender.vip_membership;
+        receiver.total_price = sender.total_price;
+
+
     }
 }
--- app/src/main/java/com/example/sungin/lab5/fragment1.java
+++ app/src/main/java/com/example/sungin/lab5/fragment1.java
@@ -12,6 +12,7 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -32,6 +33,14 @@ public class fragment1 extends Fragment{
     Table kiwi_table = new Table("kiwi");
     Table grape_table = new Table("grape");
     Table grapefruit_table = new Table("grapefruit");
+    String last_click="";
+    Table last_table = new Table("last");
+
+    DatePicker datePicker ;
+    TimePicker timePicker;
+    EditText editText_piz ;
+    EditText editText_spa ;
+    CheckBox checkBox_vip ;
 
     @Nullable
     @Override
@@ -60,30 +69,77 @@ public class fragment1 extends Fragment{
         clickTable(kiwi_table,button_kiwi);
 
 
+        setOrder(last_table,button_new);
+        setOrder(last_table,button_mod);
+        setOrder(last_table,button_reset);
 
         return v;
     }
-    public void newOrder(final Table table,Button button) {
+    public void setOrder(final Table table, final Button button) {
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View dlgview = View.inflate(getContext(), R.layout.order_dlg, null);
-                final DatePicker datePicker = (DatePicker) dlgview.findViewById(R.id.datePicker);
-                final TimePicker timePicker = (TimePicker) dlgview.findViewById(R.id.timePicker);
-                final EditText editText_piz = (EditText) dlgview.findViewById(R.id.editText_piz);
-                final EditText editText_spa = (EditText) dlgview.findViewById(R.id.editText_spa);
-                final CheckBox checkBox_vip = (CheckBox)dlgview.findViewById(R.id.checkBox_vip);
+                Toast.makeText(getActivity().getApplicationContext(), last_click , Toast.LENGTH_SHORT).show();
+                TabHost tabHost;
 
-                if (datePicker.isSelected() == true) {
-                    datePicker.setVisibility(View.GONE);
-                    timePicker.setVisibility(View.VISIBLE);
+                View dlgview = View.inflate(getContext(), R.layout.order_dlg, null);
+                tabHost = (TabHost)dlgview.findViewById(R.id.tabhost1);
+                tabHost.setup();
+
+                tabHost.addTab(tabHost.newTabSpec("A").setContent(R.id.tab1).setIndicator("날짜선택"));
+                tabHost.addTab(tabHost.newTabSpec("B").setContent(R.id.tab2).setIndicator("시간선택"));
+                tabHost.addTab(tabHost.newTabSpec("C").setContent(R.id.tab3).setIndicator("메뉴선택"));
+
+                datePicker = (DatePicker) dlgview.findViewById(R.id.datePicker);
+                timePicker = (TimePicker) dlgview.findViewById(R.id.timePicker);
+                editText_piz = (EditText) dlgview.findViewById(R.id.editText_piz);
+                editText_spa = (EditText) dlgview.findViewById(R.id.editText_spa);
+                checkBox_vip = (CheckBox)dlgview.findViewById(R.id.checkBox_vip);
+
+
+
+
+                if(button ==button_mod) {
+                    editText_piz.setText(table.piz_num);
+                    editText_spa.setText(table.spa_num);
+                    checkBox_vip.setChecked(table.vip_membership);
                 }
+                if (button == button_reset){
+                   table.putTableMenu(table,"","",false);
+                   table.putTableTime(table,0,0);
+                   table.putTableDate(table,0,0,0);
+                    table.is_void = true;
+                    text_name.setText("");
+                    text_time.setText("");
+                    text_spa.setText("");
+                    text_piz.setText("");
+                    text_mem.setText("");
+                    text_price.setText("");
+
+                    return;
+                }
+
                 AlertDialog.Builder dlg = new AlertDialog.Builder(getContext());
-                dlg.setTitle("주문정보를 입력하세요")
+                dlg.setView(dlgview)
+                        .setTitle("주문정보를 입력하세요")
                         .setPositiveButton("저장", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                table.putTable(datePicker.getYear(),datePicker.getMonth(),datePicker.getDayOfMonth(),timePicker.getHour(),timePicker.getMinute(),editText_spa.getText().toString(),editText_piz.getText().toString(), checkBox_vip.isChecked()  );
+                                table.putTableDate(table, datePicker.getYear(),datePicker.getMonth(),datePicker.getDayOfMonth());
+                                table.putTableTime(table, timePicker.getHour(),timePicker.getMinute());
+                                 table.putTableMenu(table, editText_spa.getText().toString(),editText_piz.getText().toString(), checkBox_vip.isChecked() );
+
+                                text_name.setText(table.Tablename);
+                                text_time.setText(table.year+" 0"+ table.month +" 0"+ table.day + " " +table.hour+" : "+table.min);
+                                text_spa.setText(table.spa_num);
+                                text_piz.setText(table.piz_num);
+                                if(table.vip_membership == true) text_mem.setText("VIP 멤버쉽");
+                                else text_mem.setText("기본 멤버쉽");
+                                text_price.setText(table.total_price+"원");
+
+
+
                             }
                         })
                         .setNegativeButton("취소", new DialogInterface.OnClickListener() {
@@ -94,6 +150,10 @@ public class fragment1 extends Fragment{
                         })
                         .show();
 
+                if(last_table.Tablename == apple_table.Tablename) table.putTableData(apple_table, last_table);
+                else if (last_table.Tablename == grape_table.Tablename) table.putTableData(grape_table, last_table);
+                else if(last_table.Tablename == grapefruit_table.Tablename) table.putTableData(grapefruit_table, last_table);
+                else  table.putTableData(kiwi_table, last_table) ;
             }
         });
     }
@@ -102,6 +162,7 @@ public class fragment1 extends Fragment{
             @Override
             public void onClick(View v) { //토스트 + 초기화
                 if(table.is_void ==true) {
+                    last_click = table.Tablename;
                     Toast.makeText(getActivity().getApplicationContext(), "비어있는 테이블 입니다.", Toast.LENGTH_SHORT).show();
                     text_name.setText("");
                     text_time.setText("");
@@ -112,13 +173,15 @@ public class fragment1 extends Fragment{
                 }
                 else { // table에 저장되어있는 정보입력
                     text_name.setText(table.Tablename);
-                    text_time.setText(table.year+table.month + table.day + " " +table.hour+" : "+table.min);
+                    text_time.setText(table.year+" 0"+ table.month +" 0"+ table.day + " " +table.hour+" : "+table.min);
                     text_spa.setText(table.spa_num);
                     text_piz.setText(table.piz_num);
                     if(table.vip_membership == true) text_mem.setText("VIP 멤버쉽");
                     else text_mem.setText("기본 멤버쉽");
                     text_price.setText(table.total_price+"원");
                 }
+                table.putTableData(last_table,table) ;
+
             }
         });
     }
